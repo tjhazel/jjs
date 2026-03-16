48d80dc0a8882eba8be06f14263640e416a84604
tjhazel <tjhazel@yahoo.com>
2026-03-16 16:06:36 -0600

    Cleaned up some namespaces.

--- JJS.Api/Models/Album/File.cs
+++ JJS.Api/Models/Album/File.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Text.Json.Serialization;
 
-namespace JJS.Api.Models.Album
+namespace JJS.Api.Models.Album;
+
+public class File
 {
-   public class File
-   {
-      public string Title { get; set; }
-      public string Comment { get; set; }
+   public string Title { get; set; }
+   public string Comment { get; set; }
 
-      public string Name { get; set; }
-      [JsonIgnore]
-      public string FullName { get; set; }
-      public string RelativePath { get; set; }
-      public string HttpPath { get; set; }
+   public string Name { get; set; }
+   [JsonIgnore]
+   public string FullName { get; set; }
+   public string RelativePath { get; set; }
+   public string HttpPath { get; set; }
 
-      public DateTime CreatedOn { get; set; }
-      public DateTime ModifiedOn { get; set; }
-      public DateTime LastAccessTime { get; set; }
-   }
+   public DateTime CreatedOn { get; set; }
+   public DateTime ModifiedOn { get; set; }
+   public DateTime LastAccessTime { get; set; }
 }
--- JJS.Api/Models/Album/Folder.cs
+++ JJS.Api/Models/Album/Folder.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace JJS.Api.Models.Album
+namespace JJS.Api.Models.Album;
+
+public class Folder
 {
-   public class Folder
-   {
-      public IEnumerable<Folder> Folders { get; set; }
-      public IEnumerable<File> Files { get; set; }
+   public IEnumerable<Folder> Folders { get; set; }
+   public IEnumerable<File> Files { get; set; }
 
-      public string Name { get; set; }
-      [JsonIgnore]
-      public string FullName { get; set; }
-      public string RelativePath { get; set; }
+   public string Name { get; set; }
+   [JsonIgnore]
+   public string FullName { get; set; }
+   public string RelativePath { get; set; }
 
-      public DateTime CreatedOn { get; set; }
-      public DateTime ModifiedOn { get; set; }
-      public DateTime LastAccessTime { get; set; }
-   }
+   public DateTime CreatedOn { get; set; }
+   public DateTime ModifiedOn { get; set; }
+   public DateTime LastAccessTime { get; set; }
 }
