36423aa06f1d2539e3baa663e0ce2c5446e7111d
John H <tjhazel@yahoo.com>
2026-06-15 15:54:25 -0600

    forced output distDir

--- jjs-web/next.config.ts
+++ jjs-web/next.config.ts
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV === "development") {
 
 const nextConfig: NextConfig = {
    output: 'export', //generate static site, which fails with page[id] dynamic route, so we will stick with server side rendering for now.
+   distDir: 'out',
    trailingSlash: true,
    images: {      
       dangerouslyAllowLocalIP: true, // [!code ++]
