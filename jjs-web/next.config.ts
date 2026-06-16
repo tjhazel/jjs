573ad2dc2b58b501dcd6de92506a72a6b72df618
John H <tjhazel@yahoo.com>
2026-06-16 14:45:29 -0600

    sliding next under ui folder on server.

--- jjs-web/next.config.ts
+++ jjs-web/next.config.ts
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV === "development") {
 
 const nextConfig: NextConfig = {
    output: 'export', //generate static site, which fails with page[id] dynamic route, so we will stick with server side rendering for now.
+   assetPrefix: '/ui',
    distDir: 'out',
    trailingSlash: true,
    images: {
