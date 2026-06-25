4c1a1dadb6d9abdc5d6991ad5c6e1ded1450e244
John H <tjhazel@yahoo.com>
2026-06-25 11:56:29 -0600

    updated cors settings and placeholder for ssl

--- JJS.Api/AppBuilder.cs
+++ JJS.Api/AppBuilder.cs
@@ -106,6 +106,8 @@ public class AppBuilder
                      "http://localhost:5173",
                      "http://localhost:5174",
                      "http://localhost:44301",
+                     "http://johnandjeri.com",
+                     "https://johnandjeri.com",
                      "https://*.johnandjeri.com",
                      "http://*.johnandjeri.com")
                  .SetIsOriginAllowedToAllowWildcardSubdomains()
